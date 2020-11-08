d7fad94635ac6b49df9de42a537571b422b69e8d
saahil4real <60621302+saahil4real@users.noreply.github.com>
2020-11-08 14:08:05 +0530

    pages

--- backend/index.js
+++ backend/index.js
@@ -83,6 +83,26 @@ app.get("/Teacher/:Email/:Branch/:Sid/responseStress", async function (req, res)
     res.render("responseStress.ejs", { form: form, Student_details: foundstudent, Teacher_details: foundteacher, heading: "Form 1", questions, sid });
 })
 
+app.get("/Teacher/:Email/:Branch/:Sid/responseAnxiety", async function (req, res) {
+    const sid = req.params.Sid;
+    const form = await anxietyModel.find({ Sid: sid, month: months[d.getMonth()], year: d.getFullYear() });
+    const foundstudent = await studentModel.findOne({ Sid: sid });
+    const email = req.params.Email;
+    const foundteacher = await teacherModel.findOne({ Email: email });
+    const questions = await formModel.find({ category: 'a' });
+    res.render("responseAnxiety.ejs", { form: form, Student_details: foundstudent, Teacher_details: foundteacher, heading: "Form 2", questions, sid });
+})
+
+app.get("/Teacher/:Email/:Branch/:Sid/responseDepression", async function (req, res) {
+    const sid = req.params.Sid;
+    const form = await depressionModel.find({ Sid: sid, month: months[d.getMonth()], year: d.getFullYear() });
+    const foundstudent = await studentModel.findOne({ Sid: sid });
+    const email = req.params.Email;
+    const foundteacher = await teacherModel.findOne({ Email: email });
+    const questions = await formModel.find({ category: 'd' });
+    res.render("responseDepression.ejs", { form: form, Student_details: foundstudent, Teacher_details: foundteacher, heading: "Form 3", questions, sid });
+})
+
 app.get("/Teacher/:Email/:Branch/:Sid", wrapAsync(async function (req, res, next) {
     const sid = req.params.Sid;
     const form1 = await stressModel.find({ Sid: sid });
